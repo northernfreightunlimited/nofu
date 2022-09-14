b187bbf431b1e7a2aba6fad8bee195155d371892
ajaxify <ajaxify@users.noreply.github.com>
2022-09-14 21:44:52 +0000

    deploy: 726a5f25114bf501ce4b2a56256f3aefe255699c

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -196,7 +196,11 @@ var routes = [
                 rate: STANDARD_IMPORT_FROM_JITA_RATE + 50,
                 minReward: STANDARD_IMPORT_FROM_JITA_MIN,
                 isRoundTrip: true,
-            }
+            },
+            {
+                destination: System.DP,
+                rate: STANDARD_IMPORT_FROM_JITA_RATE + 750,
+            },
         ],
     },
     {
