0b3d21ac30623631b36d198cbccc953744b5d22b
Ajaxify <78984825+ajaxify@users.noreply.github.com>
2024-06-20 20:18:22 -0700

    build: use vite for build and dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,37 @@
-import build from "@hono/vite-cloudflare-pages";
-import devServer from "@hono/vite-dev-server";
+import build from '@hono/vite-cloudflare-pages'
+import devServer, { defaultOptions } from "@hono/vite-dev-server";
 import adapter from "@hono/vite-dev-server/cloudflare";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
+  assetsInclude: ["index.html"],
+  build: {
+    outDir: "dist/",
+    ssrEmitAssets: true,
+    ssrManifest: true,
+    ssr: true,
+  },
   plugins: [
-    build(),
+    build(
+      {
+        emptyOutDir: true,
+        minify: false,
+        entry: ["src/index.ts"],
+      }
+    ),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'index.html',
+          dest: '',
+        }
+      ]
+    }),
     devServer({
       adapter,
       entry: "src/index.ts",
+      exclude: ["/", /^\/img\/.+/, /^\/css\/.+/, ...defaultOptions.exclude],
     }),
   ],
 });
