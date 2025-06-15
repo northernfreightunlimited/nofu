8349bd6ef142b0de2561a579e8d6a37e6f75a85b
snipereagle1 <me@snipereagle.xyz>
2025-06-15 12:39:24 -0600

    update routes to have full system name, add route for jf-routes endpoint

--- src/eve/systems.ts
+++ src/eve/systems.ts
@@ -1,9 +1,9 @@
 export enum System {
-  ImperialPalace = "1DQ1-A/E3OI/Irmalin/P-Z/K7D", // Can't use 1DQ as key here because it starts with a number
+  ImperialPalace = "1DQ1-A/E3OI-U/Irmalin/P-ZMZV/K7D-II", // Can't use 1DQ as key here because it starts with a number
   Forge = "Jita",
   Irmalin = "Irmalin",
   Domain = "Amarr/Ashab",
-  UALX = "UALX",
+  UALX = "UALX-3",
   GSF = "UALX JF Range",
   GEZ = "1P-WGB",
   N3 = "N3-JBX",
